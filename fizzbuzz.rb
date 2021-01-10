a8c205f31b1c3b6bbf969195c7519b5817205ea8
Mathew E Newton <elijah2895@hotmail.com>
2021-01-10 18:46:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if num == % 3
+  if num % 3 == 0
     "Fizz"
-  elsif num == % 5
+  elsif num % 5 == 0
     "Buzz"
-  elsif num == % 15
+  elsif num % 3 && 5 == 0
     "FizzBuzz"
-  else num == % 4
+  else num % 4 == 0
     nil
   end
 end
